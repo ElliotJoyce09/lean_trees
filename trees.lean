8e8c8b3e22b56fe604fbc6f65522bc99f8cca49b
danmayloveslean <danielhmay6@gmail.com>
2024-11-09 18:20:23 +0000

    Update trees.lean
    Removed what was added in the last commit as no longer useful, added functions to remove a specific edge from a cycle in a graph, as is used in my section of the proof, and would also be useful in one of my other group member's proofs.

--- trees.lean
+++ trees.lean
@@ -3,23 +3,20 @@ import Mathlib.Combinatorics.SimpleGraph.Basic
 
 namespace trees
 
--- I made this def for a path before I realised we have Paths already (I literally typed out import SimpleGraph.Path but didn't realise lol) and also before I realised strucutres were more apt but might as well put it in.
-def pathGraph : SimpleGraph ℕ where
-  Adj a b := (a ≠ b) ∧ ((a = b + 1) ∨ (b = a + 1))
-   -- Have used N here, probably shouldn't
-  symm a b h := by
-    cases h with
-    | intro left right => --splits into either case of the ∧
-      constructor
-      · apply Ne.symm left -- left is just the goal flipped, so apply symmetry of not equal to and we are done
-      · apply Or.symm right -- similar but for right goal and or
-    done
-  loopless a ha := by -- COMMENT LATER
-    dsimp at ha
-    cases ha with
-    | intro left right =>
-      exact left rfl 
-    done
+-- For whoever does part 3 the following could be useful, so I am uploading it (I'm also using it in my proof)
+
+def secondVertexInWalk {V : Type} (G : SimpleGraph V) (v : V) (p : G.Walk v v)  : V := -- returns the first vertex along a given walk from v to v in a graph G
+  p.getVert 1
+
+def firstEdgeInWalk {V : Type} (G : SimpleGraph V) (v : V) (p : G.Walk v v)  : Sym2 V := -- gets the edge between the first vertex (v) and the second vertex in a give walk from v to v in a graph G
+  s(v, (secondVertexInWalk G v p))
+
+
+def putEdgeInSet {V : Type} (x : Sym2 V) : Set (Sym2 V) := -- places the given edge (as a Sym2 V) into a set on its own
+  {x}
+
+def graphWithEdgeRemoved {V : Type} (G : SimpleGraph V) (v : V) (p : G.Walk v v) (h : p.IsCycle) : SimpleGraph V := -- Creates a subgraph of G without the first edge in a walk from v to v, v ∈ V(G)
+  G.deleteEdges (putEdgeInSet ( firstEdgeInWalk G v p ) ) -- NOTE TO DELETE LATER: this is a subgraph of G but I have just made it as a simple graph (if this is a problem later, try changing this)
 
 def Cycle {V: Type} (G: SimpleGraph V) : Prop := -- This is in SimpleGraph.Path, so are some others, do we want to strip back what we are using or just use the premade ones?
   -- should take a vertex in V and check if there is a path which starts
@@ -30,11 +27,6 @@ def Acyclic {V : Type} (G : SimpleGraph V) : Prop :=
   -- should take all verticies in V and check if that vertex has a cycle
   sorry
 
---def Tree {V: Type} (G: SimpleGraph V) : Prop :=
+def Tree {V: Type} (G: SimpleGraph V) : Prop :=
   -- should satisfy that G is Connected and Acyclic
-  --sorry
-structure Tree : Prop where -- i have based this around how connected is definied in SimpleGraph.Basic
-  protected connected (G : SimpleGraph V): G.Connected --checks if the graph is connected (connectedness is defined much the same way but for preconnected)
-  --protected acyclic (G : SimpleGraph V): G.Acyclic Will remove comments once done
-  protected [nonempty : Nonempty V] -- check there is at least 1 vertex
--- I dont think we should be using definitions for most of these. my understanding is definitions are for specific instances or like functions, and then structures are for general types, i.e all tree graphs. Feel free to correct me but this is just my current understanding
+sorry
